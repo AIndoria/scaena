ef7491bf7cba732f120f2a1a71c423b0c89bdd07
Abe Indoria <indoria@aindoria.com>
2021-03-07 03:08:45 -0700

    Render Credits in Components, Design work

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ class App extends Component {
       secondMovieCrew: [],
       firstMoviePoster: "",
       secondMoviePoster: "",
+      firstMovieRenderedCrew: [],
+      secondMovieRenderedCrew: [],
     };
     this.scrollToResults = React.createRef();
   }
@@ -38,9 +40,12 @@ class App extends Component {
       this.secondMovieRequest();
     }
 
-    this.myDivToFocus.current.scrollIntoView({
+    this.scrollToResults.current.scrollIntoView({
       behavior: "smooth",
       block: "nearest",
+    });
+    this.setState({
+      firstMovieRenderedCrew: [],
     });
   };
 
@@ -52,18 +57,11 @@ class App extends Component {
         this.state.firstMovie +
         "&page=1&include_adult=yes"
     ).then((response) => response.json());
-    this.setState(
-      {
-        firstMovieData: movieData,
-        firstMovieID: movieData.results[0].id,
-        firstMoviePoster: movieData.results[0].poster_path,
-      },
-      () => {
-        console.log(this.state.firstMovieData);
-        console.log(this.state.firstMovieID);
-        console.log(this.state.firstMoviePoster);
-      }
-    );
+    this.setState({
+      firstMovieData: movieData,
+      firstMovieID: movieData.results[0].id,
+      firstMoviePoster: movieData.results[0].poster_path,
+    });
     this.firstMovieCreditsRequest();
   };
 
@@ -71,14 +69,10 @@ class App extends Component {
     const movieCredits = await fetch(
       `https://api.themoviedb.org/3/movie/${this.state.firstMovieID}/credits?api_key=${API_KEY}&language=en-US`
     ).then((response) => response.json());
-    this.setState(
-      {
-        firstMovieCrew: movieCredits.crew,
-      },
-      () => {
-        console.log(this.state.firstMovieCrew);
-      }
-    );
+    this.setState({
+      firstMovieCrew: movieCredits.crew,
+    });
+    this.RenderNamesFirstMovie();
   };
 
   secondMovieRequest = async () => {
@@ -89,18 +83,11 @@ class App extends Component {
         this.state.secondMovie +
         "&page=1&include_adult=yes"
     ).then((response) => response.json());
-    this.setState(
-      {
-        secondMovieData: movieData,
-        secondMovieID: movieData.results[0].id,
-        secondMoviePoster: movieData.results[0].poster_path,
-      },
-      () => {
-        console.log(this.state.secondMovieData);
-        console.log(this.state.secondMovieID);
-        console.log(this.state.secondMoviePoster);
-      }
-    );
+    this.setState({
+      secondMovieData: movieData,
+      secondMovieID: movieData.results[0].id,
+      secondMoviePoster: movieData.results[0].poster_path,
+    });
     this.secondMovieCreditsRequest();
   };
 
@@ -108,19 +95,57 @@ class App extends Component {
     const movieCredits = await fetch(
       `https://api.themoviedb.org/3/movie/${this.state.secondMovieID}/credits?api_key=${API_KEY}&language=en-US`
     ).then((response) => response.json());
-    this.setState(
-      {
-        secondMovieCrew: movieCredits,
-      },
-      () => {
-        console.log(this.state.secondMovieCrew.crew);
+    this.setState({
+      secondMovieCrew: movieCredits.crew,
+    });
+    this.RenderNamesSecondMovie();
+  };
+
+  RenderNamesFirstMovie = async () => {
+    let jobs = [
+      "Director",
+      "Producer",
+      "Screenplay",
+      "Original Music Composer",
+      "Costume Design",
+      "Director of Photography",
+    ];
+    this.state.firstMovieCrew.forEach((crewMember) => {
+      if (jobs.includes(crewMember.job)) {
+        // console.log(crewMember.job + ": " + crewMember.name);
+        this.setState({
+          firstMovieRenderedCrew: this.state.firstMovieRenderedCrew.concat({
+            [crewMember.job]: [crewMember.name],
+          }),
+          // ( {"Director": ["Joseph Montegomery"], "Producer": ["Caitlyn Cow"]} )
+        });
       }
-    );
+    });
+  };
+
+  RenderNamesSecondMovie = async () => {
+    let jobs = [
+      "Director",
+      "Producer",
+      "Screenplay",
+      "Original Music Composer",
+      "Costume Design",
+      "Director of Photography",
+    ];
+    this.state.secondMovieCrew.forEach((crewMember) => {
+      if (jobs.includes(crewMember.job)) {
+        this.setState({
+          secondMovieRenderedCrew: this.state.secondMovieRenderedCrew.concat({
+            [crewMember.job]: [crewMember.name],
+          }),
+        });
+      }
+    });
   };
 
   render() {
     return (
-      <div className="App container relative flex flex-col mx-auto bg-gray-800 text-center text-gray-100 h-200 items-center overflow-hidden w-screen">
+      <div className="App container relative flex flex-col mx-auto bg-gray-800 text-center text-gray-100 h-250 items-center overflow-x-hidden w-screen">
         <div className="h-24 w-24 rounded-full absolute -right-10 -top-10 bg-gray-700"></div>
         <div className="h-44 w-44 rounded-full absolute -left-20 -top-20 bg-gray-700"></div>
         <div className="Apptitle my-8 text-lg font-medium tracking-widest text-pink-200">
@@ -173,42 +198,144 @@ class App extends Component {
           className="ResultScreen bg-gray-200 Searchbox container flex flex-col text-gray-800 w-11/12 mx-auto my-12 rounded-2xl"
           ref={this.scrollToResults}
         >
-          <div className="grid grid-cols-3">
-            <div className="grid grid-rows-5">
-              <div className="specialbox h-12"></div>
-              <div className="flex justify-center h-18 items-center bg-gray-700 text-gray-200  font-medium tracking-wide leading-relaxed">
-                {this.state.firstMovie}
-              </div>
-              <div className="flex justify-center h-17 items-center bg-gray-700 text-gray-200 font-medium tracking-wide leading-relaxed">
-                {this.state.secondMovie}
-              </div>
-              <div className="flex justify-center items-center bg-gray-700 text-gray-200 text-lg font-medium tracking-wide leading-relaxed shadow-none">
-                CREW
-              </div>
+          <div className="container grid grid-cols-3 overflow-hidden rounded-2xl ">
+            <div className="specialbox bg-gray-800"></div>
+            <div className="overflow-hidden">
+              <img
+                src={
+                  "https://image.tmdb.org/t/p/w200" +
+                  this.state.firstMoviePoster
+                }
+                alt={"Poster of " + this.state.firstMovie}
+              ></img>
             </div>
-            <div className="">
-              <div className=" overflow-hidden ">
-                <img
-                  src={
-                    "https://image.tmdb.org/t/p/w200" +
-                    this.state.firstMoviePoster
-                  }
-                  alt={"Poster of " + this.state.firstMovie}
-                ></img>
-                <div>4</div>
-              </div>
+            <div className="overflow-hidden">
+              <img
+                src={
+                  "https://image.tmdb.org/t/p/w200" +
+                  this.state.secondMoviePoster
+                }
+                alt={"Poster of " + this.state.secondMovie}
+              ></img>
             </div>
-            <div>
-              <div className="rounded-tr-2xl overflow-hidden">
-                <img
-                  src={
-                    "https://image.tmdb.org/t/p/w200" +
-                    this.state.secondMoviePoster
-                  }
-                  alt={"Poster of " + this.state.secondMovie}
-                ></img>
-              </div>
-              <div>5</div>
+            <div className="bg-gray-700 text-gray-200 text-lg font-medium tracking-wide leading-relaxed shadow-none -mt-12 flex justify-center items-center">
+              CREW
+            </div>
+            <div className=""></div>
+            <div className=""></div>
+            <div className="bg-red-200 flex justify-center items-center py-2 text-lg font-medium tracking-wide leading-relaxed">
+              Directors
+            </div>
+            <div className="flex justify-center items-center flex-col py-2 text-base font-medium tracking-wide leading-relaxed">
+              {this.state.firstMovieRenderedCrew.map((element) => {
+                if (element.Director) {
+                  console.log("Director " + element.Director);
+                  return element.Director;
+                }
+              })}
+            </div>
+            <div className="flex justify-center items-center flex-col py-2 text-base font-medium tracking-wide leading-relaxed">
+              {this.state.secondMovieRenderedCrew.map((element) => {
+                if (element.Director) {
+                  console.log("Director " + element.Director);
+                  return element.Director;
+                }
+              })}
+            </div>
+            <div className="bg-pink-200 flex justify-center items-center py-2 text-lg font-medium tracking-wide leading-relaxed">
+              Producers
+            </div>
+            <div className="flex justify-center items-center flex-col py-2 text-base font-medium tracking-wide leading-relaxed">
+              {this.state.firstMovieRenderedCrew.map((element) => {
+                if (element.Producer) {
+                  console.log("Producer " + element.Producer);
+                  return <div>{element.Producer}</div>;
+                }
+              })}
+            </div>
+            <div className="flex justify-center items-center flex-col py-2 text-base font-medium tracking-wide leading-relaxed">
+              {this.state.secondMovieRenderedCrew.map((element) => {
+                if (element.Producer) {
+                  console.log("Producer " + element.Producer);
+                  return <div>{element.Producer}</div>;
+                }
+              })}
+            </div>
+            <div className="bg-red-200 flex justify-center items-center py-2 text-lg font-medium tracking-wide leading-relaxed">
+              Screenplay
+            </div>
+            <div className="flex justify-center items-center flex-col py-2 text-base font-medium tracking-wide leading-relaxed">
+              {this.state.firstMovieRenderedCrew.map((element) => {
+                if (element.Screenplay) {
+                  console.log("Screenplay " + element.Screenplay);
+                  return element.Screenplay;
+                }
+              })}
+            </div>
+            <div className="flex justify-center items-center flex-col py-2 text-base font-medium tracking-wide leading-relaxed">
+              {this.state.secondMovieRenderedCrew.map((element) => {
+                if (element.Screenplay) {
+                  console.log("Screenplay " + element.Screenplay);
+                  return element.Screenplay;
+                }
+              })}
+            </div>
+            <div className="bg-pink-200 flex justify-center items-center py-2 text-lg font-medium tracking-wide leading-relaxed">
+              Composers
+            </div>
+            <div className="flex justify-center items-center flex-col py-2 text-base font-medium tracking-wide leading-relaxed">
+              {this.state.firstMovieRenderedCrew.map((element) => {
+                if (element["Original Music Composer"]) {
+                  console.log("Composer " + element["Original Music Composer"]);
+                  return element["Original Music Composer"];
+                }
+              })}
+            </div>
+            <div className="flex justify-center items-center flex-col py-2 text-base font-medium tracking-wide leading-relaxed">
+              {this.state.secondMovieRenderedCrew.map((element) => {
+                if (element["Original Music Composer"]) {
+                  console.log("Composer " + element["Original Music Composer"]);
+                  return element["Original Music Composer"];
+                }
+              })}
+            </div>
+            <div className="bg-red-200 flex justify-center items-center py-2 text-lg font-medium tracking-wide leading-relaxed">
+              Costume
+            </div>
+            <div className="flex justify-center items-center flex-col py-2 text-base font-medium tracking-wide leading-relaxed">
+              {this.state.firstMovieRenderedCrew.map((element) => {
+                if (element["Costume Design"]) {
+                  console.log("Costume " + element["Costume Design"]);
+                  return element["Costume Design"];
+                }
+              })}
+            </div>
+            <div className="flex justify-center items-center flex-col py-2 text-base font-medium tracking-wide leading-relaxed">
+              {this.state.secondMovieRenderedCrew.map((element) => {
+                if (element["Costume Design"]) {
+                  console.log("Costume " + element["Costume Design"]);
+                  return element["Costume Design"];
+                }
+              })}
+            </div>
+            <div className="bg-pink-200 flex justify-center items-center  py-2 text-lg font-medium tracking-wide leading-relaxed">
+              Photography
+            </div>
+            <div className="flex justify-center items-center flex-col py-2 text-base font-medium tracking-wide leading-relaxed">
+              {this.state.firstMovieRenderedCrew.map((element) => {
+                if (element["Director of Photography"]) {
+                  console.log("Cine " + element["Director of Photography"]);
+                  return element["Director of Photography"];
+                }
+              })}
+            </div>
+            <div className="flex justify-center items-center flex-col py-2 text-base font-medium tracking-wide leading-relaxed">
+              {this.state.secondMovieRenderedCrew.map((element) => {
+                if (element["Director of Photography"]) {
+                  console.log("Cine " + element["Director of Photography"]);
+                  return element["Director of Photography"];
+                }
+              })}
             </div>
           </div>
         </div>
