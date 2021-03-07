012b51e8c8b44c586df7a20ea0dc560139a3d574
Abe Indoria <indoria@aindoria.com>
2021-03-07 16:42:42 -0700

    Add Google Analytics ID

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 
 const API_KEY = process.env.REACT_APP_MDB_API_KEY; //Absolutely not secure, consider doing it on a backend Express server for actual API keys that cost money. This one is rate limited and free so it doesn't matter.
 import ReactGA from "react-ga";
-const TRACKING_ID = "264660140";
+const TRACKING_ID = "G-C9451NGMHL";
 ReactGA.initialize(TRACKING_ID);
 
 class App extends Component {
